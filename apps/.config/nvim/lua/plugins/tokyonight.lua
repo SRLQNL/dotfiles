cf783df95efe2b55a72090b5b437e1a617293d80
SRLQNL <tapo4c902@gmail.com>
2026-05-10 00:11:09 +0300

    feat: set tokyonight nvim theme

---
+++ apps/.config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon",
+      terminal_colors = true,
+      styles = {
+        sidebars = "dark",
+        floats = "dark",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
+  },
+}
